ffabbc1cffba1ce34399f6b65c0a7f331f6301a8
Léo Riera <leo.riera1@gmail.com>
2022-06-07 16:48:59 +0200

    content.js always resolve

--- content.js
+++ content.js
@@ -1,49 +1,49 @@
 function getTitle() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     chrome.storage.local.get('titleSelector', (data) => {
       if (data.titleSelector) {
         const titleSpan = document.querySelector(data.titleSelector);
         if (titleSpan !== null) {
           resolve(titleSpan.innerText);
         } else {
-          reject(new Error('Title not found'));
+          resolve(null);
         }
       } else {
-        reject(new Error('Title selector not found'));
+        resolve(null);
       }
     });
   });
 }
 
 function getDates() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     chrome.storage.local.get('datesSelector', (data) => {
       if (data.datesSelector) {
         const dateSpan = document.querySelector(data.datesSelector);
         if (dateSpan !== null) {
           resolve(dateSpan.innerText);
         } else {
-          reject(new Error('Dates not found'));
+          resolve(null);
         }
       } else {
-        reject(new Error('Dates selector not found'));
+        resolve(null);
       }
     });
   });
 }
 
 function getImage() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     chrome.storage.local.get('imageSelector', (data) => {
       if (data.imageSelector) {
         const imageElement = document.querySelector(data.imageSelector);
         if (imageElement !== null) {
           resolve(imageElement.src);
         } else {
-          reject(new Error('Image not found'));
+          resolve(null);
         }
       } else {
-        reject(new Error('Image selector not found'));
+        resolve(null);
       }
     });
   });
